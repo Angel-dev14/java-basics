47067625f60398508891d6f7df6943447bd92027
angel.petrushevski <angel.petrushevski@sorsix.com>
2024-04-17 02:36:55 +0800

    lesson 2 17.04.2024

--- src/Main.java
+++ src/Main.java
@@ -1,160 +1,110 @@
-import java.util.ArrayList;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalField;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.*;
 
 class Person {
 
-    private String name;
-    private Integer age;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
 
-    Person(String name, Integer age) {
-        //this.name = name;
-        this.name = name;
-        this.age = age;
+    public Person(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public Integer getAge() {
-        return age;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
     }
 
     @Override
     public String toString() {
-        return String.format("Name: %s Age: %s\n", this.name, this.age);
+        return String.format("%s - %s", firstName, lastName);
     }
 
     @Override
     public int hashCode() {
-        return this.name.hashCode() + this.age.hashCode();
+        return firstName.hashCode();
     }
 
     @Override
     public boolean equals(Object obj) {
-        Person other = (Person) obj;
-        if (this == other) {
-            return true;
-        }
-        System.out.println(other.name);
-        System.out.println(other.getName());
-        return this.name.equals(other.name) && this.age.equals(other.age);
+        return firstName.equals(((Person) obj).firstName);
     }
 }
 
-enum AnimalType {
-    DOG,
-    CAT
+
+interface YearOfBirthTemplateFilter {
+
+    Boolean test(LocalDate date);
+    Boolean test2(LocalDate date);
+
 }
 
-class Animal {
-
-    protected String name;
-    private AnimalType tip;
-
-    Animal(String name, AnimalType tip) {
-        this.name = name;
-        this.tip = tip;
-    }
-
-    public AnimalType getTip() {
-        return tip;
-    }
-}
-
-class Dog extends Animal {
-
-    String rasa;
-
-    Dog(String name, String rasa) {
-        super(name, AnimalType.DOG);
-        this.rasa = rasa;
-    }
-
-    public String getRasa() {
-        return rasa;
-    }
-
-    @Override
-    public String toString() {
-        return this.getTip() + " " + this.name;
-    }
-}
-
-class Cat extends Animal {
-
-    String boja;
-
-    Cat(String name, String boja) {
-        super(name, AnimalType.CAT);
-        this.boja = boja;
-    }
-
-    public String getBoja() {
-        return boja;
-    }
-
-    @Override
-    public String toString() {
-        return this.getTip() + " " + this.name;
-    }
-}
-
-//class G<E> implements List<E> {
-//
-//}
-
-// [ Animal Garden - {NAME} ]
-// [ House of animal - {name of animal} - TYPE
-// [ House of animal - {name of animal} - TYPE
-// [ House of animal - {name of animal} - TYPE
-// [ House of animal - {name of animal} - TYPE
-
-class AnimalGarden {
-
-    List<Animal> animals;
-
-    AnimalGarden() {
-        this.animals = new ArrayList<Animal>();
-    }
-
-    void addAnimal(Animal animal) {
-        this.animals.add(animal);
-    }
-
-    void removeAnimal(Animal animal) throws RuntimeException {
-        if(this.animals.contains(animal)) {
-            this.animals.remove(animal);
-        }
-        throw new RuntimeException("There is no animal with name " + animal.name);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < animals.size(); i++) {
-            builder.append(animals.get(i).toString());
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Animal dog1 = new Dog("pero", "vucjak");
-        Animal dog2 = new Dog("pero", "vucjak");
-        Animal dog3 = new Dog("pero", "vucjak");
-        Animal cat = new Cat("pero", "bela");
-        Animal[] animals = {dog1, dog2, dog3, cat};
+        // Set
+        Set<Person> personSet = new HashSet<Person>();
+        Person person1 = new Person("Angel", "Pet", LocalDate.of(2001, 5, 14));
+        Person person2 = new Person("Angelaa", "Pet", LocalDate.of(2000, 5, 14));
+        Person person3 = new Person("Angelaaaa", "Pet", LocalDate.of(1999, 5, 14));
+        Person person4 = new Person("Kiko", "Kikov", LocalDate.of(2002, 7, 22));
+        personSet.add(person1);
+        personSet.add(person3);
+        personSet.add(person4);
+        personSet.add(person2);
 
-        AnimalGarden garden = new AnimalGarden();
-        garden.addAnimal(dog1);
-        garden.addAnimal(dog2);
-        garden.addAnimal(dog3);
-        garden.addAnimal(cat);
-        System.out.println(garden);
+        // filter out persons that are born after 2001 godina
+
+        // group persons with same surname, and the year of births
+
+        Predicate<Person> p = person -> person.getDateOfBirth().getYear() <= 2001;
+
+
+        Predicate<Person> pred = person -> filter.test(person.getDateOfBirth());
+
+        Map<String, Set<Integer>> persons = personSet.stream().filter(p)
+                .collect(
+                    Collectors.groupingBy(
+                        Person::getLastName, Collectors.mapping(p -> p.getDateOfBirth().getYear(), Collectors.toSet())
+                    )
+                );
+
+        System.out.println(
+            persons
+        );
+
+    //    https://www.baeldung.com/java-groupingby-collector
+
     }
 }
